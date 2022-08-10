19b336b63ad0492c37e43bd4bc3969423a60328d
RFC1918 <61937657+RFC1918-hub@users.noreply.github.com>
2022-08-10 16:28:52 +0200

    Create manage_azure_vms.ps1

---
+++ manage_azure_vms.ps1
@@ -0,0 +1,17 @@
+[CmdletBinding()]
+    Param (
+        [Switch]
+        $shutdown,
+        [Switch]
+        $boot
+    )
+
+Get-AzContext | Format-List
+
+if ($shutdown) {
+    Get-AzVM -Status | ForEach-Object {Stop-AzVM -ResourceGroupName $_.ResourceGroupName -Name $_.Name -Force}
+} elseif ($boot) {
+    Get-AzVM -Status | ForEach-Object {Start-AzVM -ResourceGroupName $_.ResourceGroupName -Name $_.Name -Force}
+} else {
+    Get-AzVM -Status
+}
